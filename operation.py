d4a1feb1600e7440e636c3675695df5ea5d48d2e
Bertrand <bertrand-7@hotmail.com>
2022-09-14 12:15:52 +0200

    Modify path better visbility, separate log file

--- operation.py
+++ operation.py
@@ -1,30 +1,20 @@
-
-from genericpath import isfile
-import logging
-import sys
 import os 
 import shutil
-import sys
-
-
 
 #Function to delete extra files in Replica folder (the ones no present in the source folder)
 def deleteFiles(list,logger,repF):
     for i in list:
-        fullPath = repF + "/"+i
+        fullPath = os.path.abspath(repF + "/"+i)
         if os.path.isfile(fullPath):
             os.remove(fullPath)
         elif os.path.isdir(fullPath):
             shutil.rmtree(fullPath)
           
-        logger.info("[DELETED] " + "'"+ i +"'"+ " was removed from " + fullPath)
+        logger.info("[DELETED] " + "'"+ i +"'"+ " was removed from " + os.path.abspath(repF))
 
 
 # When a file or folder is not existing in replica, but in the source folder. A copy will be done added in replica folder
 def createFiles(list,logger,srcF,repF):
-    print(list)
-    print(srcF)
-    print(repF)
     for i in list:
         fullPathS = os.path.abspath(srcF + "/" +i)
         fullPathR = os.path.abspath(repF + "/" + i)
@@ -32,7 +22,7 @@ def createFiles(list,logger,srcF,repF):
             shutil.copy(fullPathS, fullPathR)
         elif os.path.isdir(fullPathS):
             shutil.copytree(fullPathS, fullPathR)
-        logger.info("[CREATED] " + "'"+ i +"'"+ " was created in " + fullPathR)  
+        logger.info("[CREATED] " + "'"+ i +"'"+ " was created in " + os.path.abspath(repF))  
 
 
 # Update function will remove the folder or file from replica folder and copy the version from the source folder
